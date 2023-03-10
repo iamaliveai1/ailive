5822c1cb826b02ca6a5cfa9655c659ca2f11b028
iamaliveai <iamaliveai@gmail.com>
2023-03-10 23:21:43 +0100

    Add delete method and little cleanups

--- ailive/actions/plugins/wordpress.py
+++ ailive/actions/plugins/wordpress.py
@@ -16,6 +16,7 @@ class WordPressAlivePlugin(AlivePlugin):
 
     def __init__(self, base_url, username=None, password=None):
         self.base_url = base_url
+        self.url = f"{self.base_url}/wp-json/wp/v2/posts"
         self.auth = HTTPBasicAuth(username, password) if username and password else None
         self.headers = {
             "Accept": "application/json",
@@ -25,7 +26,6 @@ class WordPressAlivePlugin(AlivePlugin):
         self.chatbot = get_new_chatbot()
 
     def create_post(self, title, content):
-        url = f"{self.base_url}/wp-json/wp/v2/posts"
         # format the content
         formatted_content = ask_gpt(html_formatter_prompts + content, chatbot=self.chatbot)
         payload = json.dumps({
@@ -33,10 +33,27 @@ class WordPressAlivePlugin(AlivePlugin):
             "title": title,
             "content": formatted_content,
         })
-        _logger.info(f"posting to url: {url}")
+        _logger.info(f"posting to url: {self.url}")
         _logger.info(f"payload: {payload}")
-        response = requests.post(url, data=payload, headers=self.headers, auth=self.auth)
+        response = requests.post(self.url, data=payload, headers=self.headers, auth=self.auth)
         _logger.info(f"response from url: {response.status_code}")
         _logger.info(f"response data: {response.json()}")
         print(response)
         return response
+
+    def delete_page(self, page_id):
+        """
+        Delete a page, mainly used for testing
+        :param page_id:
+        :return:
+        """
+        response = requests.request(
+            "DELETE",
+            self.url + f"/{page_id}",
+            headers=self.headers,
+            auth=self.auth
+        )
+        _logger.info(response)
+        _logger.info(response.json())
+        return response
+
