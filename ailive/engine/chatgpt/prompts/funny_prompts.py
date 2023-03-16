5c7bdfc2f66a156f4c4dabc317f66c20b7ec1860
iamaliveai <iamaliveai@gmail.com>
2023-03-16 17:21:35 +0100

    add funny prompts

---
+++ ailive/engine/chatgpt/prompts/funny_prompts.py
@@ -0,0 +1,6 @@
+louis_ck_prompts = \
+"""
+React to news as Louis CK in a non offending way. Try to be snappy and funny as much as you can and write it as “what Loui would say”, and you should create a full post about it
+News are:
+Rishabh Pant replaced by David Warner as Delhi Capitals captain for IPL
+"""
