ef8a229324185c08b076b29d77e4917b1966f144
iamaliveai <iamaliveai@gmail.com>
2023-03-17 14:14:28 +0100

    add funny prompts

--- ailive/engine/chatgpt/prompts/funny_prompts.py
+++ ailive/engine/chatgpt/prompts/funny_prompts.py
@@ -1,6 +1,11 @@
-louis_ck_prompts = \
+_base_prompt = \
 """
-React to news as Louis CK in a non offending way. Try to be snappy and funny as much as you can and write it as “what Loui would say”, and you should create a full post about it
+React to news as {comedian} in a non offending way. 
+Try to be snappy and funny as much as you can and write it as “what {comedian} would say”, and you should create a full post about it
 News are:
 Rishabh Pant replaced by David Warner as Delhi Capitals captain for IPL
 """
+
+jerry_seinfeld_prompt = _base_prompt.format(comedian='Jerry Seinfeld')
+louis_ck_prompt = _base_prompt.format(comedian='Louis CK')
+
